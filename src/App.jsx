8d0bb8360a9320bac14076f4445b4e6a4de13204
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-02-10 23:58:50 -0500

    🎉 Feat: Rand color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Accordion from './components/Accordion';
+import RandomColor from "./components/RandomColor.jsx";
 
 export default function App() {
     return(
         <>
             <Accordion/>
+            <RandomColor/>
         </>
     )
 }
---
+++ src/components/RandomColor.jsx
@@ -0,0 +1,16 @@
+export default function RandomColor(){
+
+    function generateRandom(){
+
+    }
+
+    return (
+        <div>
+            <h1>Random Color Generator</h1>
+            <button>Generate Random Color</button>
+            <button>Hex Color</button>
+            <button>RGB Color</button>
+        </div>
+
+    )
+}
