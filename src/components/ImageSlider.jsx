ce245351357423ae9763cd207ac14670e06a4320
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-02-18 22:30:30 -0500

    💄 Style: Add buttons

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -16,7 +16,7 @@ export default function ImageSlider({count=5}) {
                 }
                 return response.json()
             }).then(data => {
-                    setImages(data.map((image) => image.urls.raw));
+                    setImages(data.map((image) => image.urls.small));
                 }
             ).catch((error) => {
                 console.error(error);
@@ -34,19 +34,74 @@ export default function ImageSlider({count=5}) {
     return (
         <div className="image-slider">
             <h1>Image Slider</h1>
-            <img src={images[currentImage]} alt="random"/>
-            <button
-                onClick={() => {
-                    if (currentImage > 0) {
-                        setCurrentImage(currentImage - 1);
-                    }
-                }}>Prev</button>
-            <button
-                onClick={() => {
-                    if (currentImage < images.length - 1) {
-                        setCurrentImage(currentImage + 1);
-                    }
-            }}>Next</button>
+            <div className="slider">
+                <img src={images[currentImage]} alt="random"/>
+                <div className="relative bottom-1/2 flex justify-between mx-4">
+                    <button
+                        onClick={() => {
+                            if (currentImage > 0) {
+                                setCurrentImage(currentImage - 1);
+                            }
+                        }}>
+                        <LeftArrow/>
+                    </button>
+                    <button
+                        onClick={() => {
+                            if (currentImage < images.length - 1) {
+                                setCurrentImage(currentImage + 1);
+                            }
+                        }}>
+                        <RightArrow/>
+                    </button>
+                </div>
+            </div>
         </div>
+    )
+}
+
+export function LeftArrow(props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 14 14"
+            width="3em"
+            height="3em"
+            {...props}
+        >
+            <g
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <rect width="14" height="14" fill="white" opacity="0.2" rx="7" />
+                <path d="M8 4L5 7l3 3"></path>
+                <circle cx="7" cy="7" r="6.5"></circle>
+            </g>
+        </svg>
+    )
+}
+export function RightArrow(
+    props,
+) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 14 14"
+            width="3em"
+            height="3em"
+            {...props}
+        >
+            <g
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <rect width="14" height="14" fill="white" opacity="0.2" rx="7" />
+                <path d="m6 4l3 3l-3 3"></path>
+                <circle cx="7" cy="7" r="6.5"></circle>
+            </g>
+        </svg>
     )
 }
