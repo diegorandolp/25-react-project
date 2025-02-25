34ffb4a0939b66200608319df9a4135d032b15b4
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-02-24 23:07:07 -0500

    💥 Feat: organize products

--- src/components/LoadMoreProducts.jsx
+++ src/components/LoadMoreProducts.jsx
@@ -2,7 +2,13 @@ import {useState, useEffect} from "react";
 
 const API_URL = import.meta.env.VITE_PLATZI_URL;
 
-export default function LoadMoreProducts(){
+export default function LoadMoreProducts({maxProductsPerRow = 4}) {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [arrayRows, setArrayRows] = useState([]);
+
     useEffect(() => {
         const controller = new AbortController();
         const signal = controller.signal
@@ -10,13 +16,13 @@ export default function LoadMoreProducts(){
         async function getProducts(){
             try {
                 setLoading(true);
-                const apiUrlPagination = `${API_URL}?offset=0&limit=10`;
+                const apiUrlPagination = `${API_URL}?offset=0&limit=20`;
                 const response = await fetch(apiUrlPagination, {signal});
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const result = await response.json();
-                console.log(result)
+                setArrayRows(Array.from(Array(result.length / maxProductsPerRow), (_, i) => i*4));
                 setProducts(result);
             } catch (error){
                 if (error.name !== 'AbortError') {
@@ -33,16 +39,21 @@ export default function LoadMoreProducts(){
         }
     }, []);
 
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+
 
     return (
-        <div>
-            {products.map((item, key) => (
-                <p>{item.title}</p>
+        <div className="load-more-products">
+            {arrayRows.map((item, key) => (
+
+                <div className="flex">
+                    {products.slice(item, item + maxProductsPerRow).map((product, key) => (
+                        <div className="product-card" key={key}>
+                            <p>{product.title}</p>
+                        </div>
+                    ))}
+                </div>
             ))}
-            <p>{error}</p>
+
         </div>
     )
 }
