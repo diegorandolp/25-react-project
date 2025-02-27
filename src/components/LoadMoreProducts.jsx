d1d48dc9dd120c1a4e3d99d230d9d1712868819e
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-02-26 22:55:16 -0500

    ✨ Feat: Load more

--- src/components/LoadMoreProducts.jsx
+++ src/components/LoadMoreProducts.jsx
@@ -8,38 +8,55 @@ export default function LoadMoreProducts({maxProductsPerRow = 4}) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [arrayRows, setArrayRows] = useState([]);
+    const [limit, setLimit] = useState(20);
+
+
+    async function getProducts(limit_, signal_){
+        try {
+            setLoading(true);
+            const apiUrlPagination = `${API_URL}?offset=0&limit=${limit_}`;
+            const response = await fetch(apiUrlPagination, {signal_});
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            setArrayRows(Array.from(Array(Math.floor(result.length / maxProductsPerRow)), (_, i) => i*4));
+            setProducts(result);
+        } catch (error){
+            if (error.name !== 'AbortError') {
+                console.log('Fetch error:', error.message);
+                console.log('Fetch aborted');
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal
 
-        async function getProducts(){
-            try {
-                setLoading(true);
-                const apiUrlPagination = `${API_URL}?offset=0&limit=20`;
-                const response = await fetch(apiUrlPagination, {signal});
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const result = await response.json();
-                setArrayRows(Array.from(Array(result.length / maxProductsPerRow), (_, i) => i*4));
-                setProducts(result);
-            } catch (error){
-                if (error.name !== 'AbortError') {
-                    console.log('Fetch aborted');
-                }
-            } finally {
-                setLoading(false);
-            }
-        }
-        getProducts()
+        const controller = new AbortController();
+        const signal = controller.signal;
+        getProducts(limit,signal);
 
         return () => {
             controller.abort();
         }
     }, []);
 
+    function handleLoadMore(){
+        if (limit < 100) {
+            setLimit((limit) => {
+                let newLimit = limit + 20;
 
+                const controller = new AbortController();
+                const signal = controller.signal;
+                getProducts(newLimit,signal);
+
+                return limit + 20
+            });
+
+        }
+    }
 
     return (
         <div className="load-more-products">
@@ -58,7 +75,7 @@ export default function LoadMoreProducts({maxProductsPerRow = 4}) {
                     ))}
                 </div>
             ))}
-
+            <button onClick={() => handleLoadMore()}>Load more products...</button>
         </div>
     )
 }
